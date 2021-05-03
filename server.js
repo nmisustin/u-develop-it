29d9f853ee3e785c106f668820bb1fec4a92e922
nmisustin <nmisustin@gmail.com>
2021-05-03 09:58:33 -0400

    Wrote and commented out db querys

--- server.js
+++ server.js
@@ -13,9 +13,30 @@ const db = mysql.createConnection(
 const app = express();
 app.use(express.urlencoded({exteneded: false}));
 app.use(express.json());
-db.query("SELECT * FROM candidates", (err, rows) => {
-    console.log(rows);
-});
+// db.query("SELECT * FROM candidates", (err, rows) => {
+//     console.log(rows);
+// });
+// db.query("SELECT * FROM candidates WHERE id=1", (err, row) => {
+//     if(err){
+//         console.log(err);
+//     }
+//     console.log(row);
+// })
+// db.query("DELETE FROM candidates WHERE id=?", 1, (err, result) => {
+//     if(err){
+//         console.log(err);
+//     }
+//     console.log(result);
+// })
+// const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
+//             VALUES (?,?,?,?)`;
+// const params = [1, "Ronald", "Firbank", 1];
+// db.query(sql, params, (err, result) => {
+//     if(err){
+//         console.log(err);
+//     }
+//     console.log(result);
+// })
 app.use((req, res) => {
     res.status(404).end();
 })
