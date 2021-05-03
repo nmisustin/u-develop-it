4badf7f6be1ef2b5fdf50acb5dbb49e587d06eb6
nmisustin <nmisustin@gmail.com>
2021-05-03 09:39:19 -0400

    added conection to the election database in the server

--- server.js
+++ server.js
@@ -1,12 +1,24 @@
 const express = require("express");
 const PORT = process.env.PORT || 3001;
+const mysql = require("mysql2");
+const db = mysql.createConnection(
+    {
+        host: "localhost",
+        user: "root",
+        password: "6812775Nm*",
+        database: "election"
+    },
+    console.log("Connected to the election database.")
+);
 const app = express();
 app.use(express.urlencoded({exteneded: false}));
 app.use(express.json());
-
+db.query("SELECT * FROM candidates", (err, rows) => {
+    console.log(rows);
+});
 app.use((req, res) => {
     res.status(404).end();
 })
 app.listen(PORT, ()=>{
-    console.log(`Server running on prot ${PORT}`);
+    console.log(`Server running on port ${PORT}`);
 })
